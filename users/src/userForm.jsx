7b50f54c994da8ac15b8e3cf2b10a9a4065403e7
lewKcoder <7kamimura74@gmail.com>
2023-07-05 11:12:16 +0900

    Fixed userForm

--- users/src/userForm.jsx
+++ users/src/userForm.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
+
 export const UserForm = () => {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+
+  const handleSubmi = (event) => {
+    event.preventDefault();
+
+    console.log(email, name);
+  };
+
   return (
-    <form action="">
+    <form action="" onSubmit={handleSubmi}>
       <div>
         <label htmlFor="">Name</label>
-        <input type="text" />
+        <input type="text" onChange={(e) => setName(e.target.value)} />
       </div>
       <div>
-        <label htmlFor="">Name</label>
-        <input type="text" />
+        <label htmlFor="">Email</label>
+        <input type="text" onChange={(e) => setEmail(e.target.value)} />
       </div>
       <button>Add User</button>
     </form>
