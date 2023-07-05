6b40cf8ea5448f437c001f38bf2001d179b58040
lewKcoder <7kamimura74@gmail.com>
2023-07-05 11:09:00 +0900

    Added userForm

---
+++ users/src/userForm.jsx
@@ -0,0 +1,15 @@
+export const UserForm = () => {
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="">Name</label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor="">Name</label>
+        <input type="text" />
+      </div>
+      <button>Add User</button>
+    </form>
+  );
+};
